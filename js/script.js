6e00c3ff128dfae761033302f41438a03ba41935
Hari <user@DESKTOP-65AVN6U>
2022-08-29 13:14:14 -0400

    added back to top button

--- js/script.js
+++ js/script.js
@@ -1,10 +1,34 @@
-$(document).ready(function(){
-    $('.carousel').slick({
-    slidesToShow: 3,
-    dots:true,
-    centerMode: true,
-    });
+$(document).ready(function () {
 
-  });
-  
   // Slick version 1.5.8
+  $('.carousel').slick({
+    slidesToShow: 3,
+    dots: true,
+    centerMode: true,
+  });
+
+  //Get the button
+  var mybutton = document.getElementById("backtotop-btn");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () {
+    scrollFunction()
+  };
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+
+});
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
